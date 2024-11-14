e181d0ebcc292b300f921230a164cc6154538f76
Kristofer Sartorial <kristofs@si-o4.kzn.21-school.ru>
2024-11-14 15:43:19 +0300

    Проект создан

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kristofer Sartorial on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
